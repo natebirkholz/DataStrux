da6b6bb9f929e5d24a4fafd5b5df567e11e852b1
natebirkholz <nbirkholz@gmail.com>
2014-12-01 13:01:47 -0800

    more work in linked list

--- LinkedList.playground/section-1.swift
+++ LinkedList.playground/section-1.swift
@@ -4,16 +4,12 @@ import UIKit
 
 var str = "Hello, playground"
 
-class Node <T: Equatable> : NilLiteralConvertible {
+class Node <T: Equatable> {
   var value : T
   var next : Node?
 
   init (value: T) {
     self.value = value
-  }
-
-  required init(nilLiteral: ()) {
-
   }
 
 }
@@ -25,57 +21,80 @@ class LinkedList <T:Equatable> {
     if self.first == nil {
       self.first = Node(value: value)
       println("created")
-    } else if self.first?.next == nil {
-      self.first?.next = Node(value: value)
-      println("added")
     } else {
-      var node = self.first!
-      while node.next? != nil {
-        node.next = node.next?.next
-        println("iterated")
-      }
-      node.next = Node(value: value)
-      println("appended \(value)")
+      var newNode  = Node(value: value)
+      newNode.next = first
+      first = newNode
+      println("added")
     }
   }
 
-  func pop ()-> T {
-    var value: T?
+  func pop ()-> T? {
+    var val: T?
+
     if self.first? == nil {
       println("empty list")
-      return nil
+      val = nil
     } else {
-      println("here")
-      var current = self.first?
-      var nextN = self.first?.next
-      println("current: \(current?.value)")
-
-      while nextN != nil {
-        println("iterated more")
-        current = nextN?
-        nextN = nextN?.next?.next
-      }
-      value = current?.value
-      current = nextN
-
+      val = self.first?.value
+      self.first = self.first?.next
     }
-    return value!
-
+    return val
   }
 
+  func enqueue(value: T) {
+    if self.first == nil {
+      self.first = Node(value: value)
+      println("created")
+    } else {
+      var newNode  = Node(value: value)
+      newNode.next = first
+      first = newNode
+      println("added")
+    }
+  }
 
+  func dequeue() -> T? {
+    var val : T?
+    if self.first? == nil {
+      println("empty queue")
+      return nil
+    } else if self.first?.next? == nil {
+      val = self.first?.value
+      self.first = self.first?.next
+    } else {
+      var previous : Node<T>?
+      var current = self.first
 
+      while current?.next != nil {
+        println("current is \(current?.value) and previous is \(previous?.value)")
+        previous = current
+        current = current?.next
+        println("current is \(current?.value) and previous is \(previous?.value)")
+      }
+      val = current?.value
+      previous?.next = current?.next
+      println("current is \(current?.value) and previous is \(previous?.value)")
+    }
+    println("-----------------")
+    return val
+  }
 
-
-}
+} // End
 
 var ll = LinkedList<String>()
 var lk = LinkedList<String>()
 
-ll.push("This")
-ll.push("Is")
-ll.push("A")
-ll.push("List")
-var this = ll.pop()
-var mine = lk.pop()
+ll.enqueue("This")
+ll.enqueue("Is")
+ll.enqueue("A")
+ll.enqueue("List")
+
+let aA = ll.dequeue()
+let bB = ll.dequeue()
+let cC = ll.dequeue()
+let dD = ll.dequeue()
+let eE = ll.dequeue()
+
+
 
